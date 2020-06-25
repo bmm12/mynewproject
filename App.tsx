4ec9754ad4218911563e2e961611c4b7465edb72
inf_bmadhuri <inf_bmadhuri@TractorSupply.com>
2020-06-25 22:18:08 +0530

    added date

--- App.tsx
+++ App.tsx
@@ -5,41 +5,44 @@ import Runner from './screens/runPlayerCard';
 import Winner from './screens/winPlayerCard';
 import Score from './screens/runScoreCard'
 
-export interface Player{
-    country:string;
-    id:string;
-    name:string;
-    scores1:any;
-    scores2:any;
-    image?:string;
-    flag?:string;
-    seed?:string;
-    profileURL?:string;
-}
-export interface PlayersData{
-  team1:Player[];
-  team2:Player[];
-}
-export interface State{
-  isLoading:any,
-  dataSource:any,
-  dataSource2:any
-}
-
-class App extends React.Component{
-  state={
-    isLoading:true,
-    dataSource:[],
-    dataSource2:[],
-  }  
-
-  player: Player={
-    country:'',
-    id:'',
-    name:'',
-    scores1:[],
-    scores2:[]
+  export interface Player{
+      country:string;
+      id:string;
+      name:string;
+      scores1:any;
+      scores2:any;
+      image?:string;
+      flag?:string;
+      seed?:string;
+      profileURL?:string;
   }
+  export interface PlayersData{
+    team1:Player[];
+    team2:Player[];
+  }
+  export interface State{
+    isLoading:any,
+    dataSource:any,
+    dataSource2:any,
+    dates:any
+  }
+
+  class App extends React.Component{
+    state={
+      isLoading:true,
+      dataSource:[],
+      dataSource2:[],
+      dates:[]
+    }  
+
+    player: Player={
+      country:'',
+      id:'',
+      name:'',
+      scores1:[],
+      scores2:[],
+    
+    }
 
   componentDidMount(){
     return fetch('https://private-10425-bmm12.apiary-mock.com/questions')
@@ -54,44 +57,43 @@ class App extends React.Component{
     .catch((error)=>{
       console.log(error)
     })
-    
   }
-    renderItems=():any=>{
-    
-      let choices:any=[];
-      this.state.dataSource.map((val:any,key:any)=>{
-      choices.push(
-        <ScrollView style={{flexDirection:'column'}} key={key}>
-            <View>
-                <Text>{''}</Text>
-                <Text style={{color:'#639080',fontWeight:'bold'}}>{val.game}</Text>
-                <Text>{val.title}</Text>
-                <Text>{''}</Text>
-            </View>
-            <View key={key} style={{flexDirection:'row'}} >
-                <Score><Text style={{fontSize:9}}>{val.country}</Text></Score>
-                <Runner><Text key={key}>{val.players.name1}[{val.seed1}]</Text></Runner>
-                <Score><Text>{val.scores1.set1}</Text></Score>
-                <Score><Text>{val.scores1.set2}</Text></Score>
-                <Score><Text>{val.scores1.set3}</Text></Score>
-                <Score><Text>{val.scores1.set4}</Text></Score>
-                <Score><Text>{val.scores1.set5}</Text></Score>
-
-            </View>
-            <View style={{flexDirection:'row'}} >
-                <Card><Text style={{fontSize:9}}>{val.coun2}</Text></Card>
-                <Winner><Text style={{color:'#ffff',fontWeight:'bold'}}>{val.players.name2}[{val.seed2}]</Text></Winner>
-                <Card><Text>{val.scores2.set1}</Text></Card>
-                <Card><Text>{val.scores2.set2}</Text></Card>
-                <Card><Text>{val.scores2.set3}</Text></Card>
-                <Card><Text>{val.scores2.set4}</Text></Card>
-                <Card><Text>{val.scores1.set5}</Text></Card>
-
-            </View> 
-        </ScrollView>
-      )
-    })
-    return choices
+  
+  renderItems=():any=>{
+  
+    let choices:any=[];
+    this.state.dataSource.map((val:any,key:any)=>{
+    choices.push(
+      <ScrollView style={{flexDirection:'column'}} key={key}>
+          <View>
+              <Text style={{fontWeight:'bold',textAlign:'center'}}>{val.date}</Text>
+              <Text>{''}</Text>
+              <Text style={{color:'#639080',fontWeight:'bold'}}>{val.game}</Text>
+              <Text>{val.title}</Text>
+              <Text>{''}</Text>
+          </View>
+          <View key={key} style={{flexDirection:'row'}} >
+              <Score><Text style={{fontSize:9}}>{val.country}</Text></Score>
+              <Runner><Text key={key}>{val.players.name1}[{val.seed1}]</Text></Runner>
+              <Score><Text>{val.scores1.set1}</Text></Score>
+              <Score><Text>{val.scores1.set2}</Text></Score>
+              <Score><Text>{val.scores1.set3}</Text></Score>
+              <Score><Text>{val.scores1.set4}</Text></Score>
+              <Score><Text>{val.scores1.set5}</Text></Score>
+          </View>
+          <View style={{flexDirection:'row'}} >
+              <Card><Text style={{fontSize:9}}>{val.coun2}</Text></Card>
+              <Winner><Text style={{color:'#ffff',fontWeight:'bold'}}>{val.players.name2}[{val.seed2}]</Text></Winner>
+              <Card><Text>{val.scores2.set1}</Text></Card>
+              <Card><Text>{val.scores2.set2}</Text></Card>
+              <Card><Text>{val.scores2.set3}</Text></Card>
+              <Card><Text>{val.scores2.set4}</Text></Card>
+              <Card><Text>{val.scores1.set5}</Text></Card>
+          </View> 
+      </ScrollView>
+    )
+  })
+  return choices
   }
   renderItems1=():any=>{
     
@@ -99,6 +101,7 @@ class App extends React.Component{
     this.state.dataSource2.map((val:any,key:any)=>{
     choices1.push(
       <ScrollView style={{flexDirection:'column'}} key={key}>
+          <Text style={{fontWeight:'bold',textAlign:'center'}}>{val.date}</Text>
           <View><Text>{''}</Text>
             <Text style={{color:'#639080',fontWeight:'bold'}}>{val.game}</Text>
             <Text>{val.title}</Text><Text>{''}</Text>
@@ -128,9 +131,8 @@ class App extends React.Component{
     )
   })
   return choices1
-}
+  }
   render(){
-    console.log(this.state.dataSource,this.state.dataSource2)
           if(this.state.isLoading){
             return(
               <View style={styles.container}>
@@ -149,8 +151,7 @@ class App extends React.Component{
             }
   }
 
-
-const styles = StyleSheet.create({
+  const styles = StyleSheet.create({
   
   container: {
     flex: 1,
