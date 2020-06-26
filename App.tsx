4e259cf8b53590d38f795b57838e2226e70a676f
inf_bmadhuri <inf_bmadhuri@TractorSupply.com>
2020-06-26 15:06:40 +0530

    style file added

--- App.tsx
+++ App.tsx
@@ -4,6 +4,9 @@ import Card from './screens/winScoreCard';
 import Runner from './screens/runPlayerCard';
 import Winner from './screens/winPlayerCard';
 import Score from './screens/runScoreCard'
+import { inputStyle } from './style';
+
+const styles = StyleSheet.create(inputStyle);
 
   export interface Player{
       country:string;
@@ -152,61 +155,5 @@ import Score from './screens/runScoreCard'
               }
             }
   }
-
-  const styles = StyleSheet.create({
-  
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor:'#fff',
-  },
-  namewinsingles:{
-    color:'#ffff',
-    fontWeight:'bold',
-    fontSize:14,
-    lineHeight:18,
-    marginVertical:14,
-    marginHorizontal:4
-  },
-  namesingles:{
-    marginHorizontal:4,
-    marginVertical:14,
-  },
-  doubleview:{
-    marginVertical:4,
-    marginHorizontal:4
-  },
-  doubleswin:{
-    color:'#ffff',
-    fontWeight:'bold',
-    fontSize:14,
-    lineHeight:18,
-  },
-  namedoubles:{
-    marginHorizontal:4,
-    marginVertical:4,
-  },
-  score:{
-    marginVertical:14,
-    marginHorizontal:10
-  },
-  scorewin:{
-    marginVertical:14,
-    marginHorizontal:10,
-    color:'#ffff',
-    fontWeight:'bold',
-    fontSize:14,
-    lineHeight:18,
-  },
-  game:{
-    color:'#639080',
-    fontWeight:'bold'
-  },
-  date:{
-    fontWeight:'bold',
-    textAlign:'center'
-  },
-});
 
 export default App
---
+++ style.tsx
@@ -0,0 +1,56 @@
+export const inputStyle:any={
+  
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor:'#fff',
+    },
+    namewinsingles:{
+      color:'#ffff',
+      fontWeight:'bold',
+      fontSize:14,
+      lineHeight:18,
+      marginVertical:14,
+      marginHorizontal:4
+    },
+    namesingles:{
+      marginHorizontal:4,
+      marginVertical:14,
+    },
+    doubleview:{
+      marginVertical:4,
+      marginHorizontal:4
+    },
+    doubleswin:{
+      color:'#ffff',
+      fontWeight:'bold',
+      fontSize:14,
+      lineHeight:18,
+    },
+    namedoubles:{
+      marginHorizontal:4,
+      marginVertical:4,
+    },
+    score:{
+      marginVertical:14,
+      marginHorizontal:10
+    },
+    scorewin:{
+      marginVertical:14,
+      marginHorizontal:10,
+      color:'#ffff',
+      fontWeight:'bold',
+      fontSize:14,
+      lineHeight:18,
+    },
+    game:{
+      color:'#639080',
+      fontWeight:'bold'
+    },
+    date:{
+      fontWeight:'bold',
+      textAlign:'center'
+    },
+  }
+
